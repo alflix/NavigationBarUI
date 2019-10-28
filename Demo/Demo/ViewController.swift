89ce9a7ac17c149883c446268db0d0ac68bca1bd
alflix <jieyuanz24@gmail.com>
2019-10-28 19:02:38 +0800

    update demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -12,9 +12,44 @@ import Reusable
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationAppearance.barTintColor = .white
+    }
+
+    @IBAction func colorValueChange(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            navigationAppearance.barTintColor = .green
+        case 1:
+            navigationAppearance.barTintColor = .red
+        case 2:
+            navigationAppearance.barTintColor = .white
+        case 3:
+            navigationAppearance.barTintColor = .blue
+        case 4:
+            navigationAppearance.barTintColor = .black
+        default:
+            ()
+        }
+    }
+
+    @IBAction func shadowHiddenValueChange(_ sender: UISwitch) {
+        navigationAppearance.showShadowLine = !sender.isOn
+    }
+
+    @IBAction func barHiddenValueChange(_ sender: UISwitch) {
+        navigationAppearance.isNavigationBarHidden = sender.isOn
+    }
+
+    @IBAction func blackBarStyleValueChange(_ sender: UISwitch) {
+
     }
 
     @IBAction func sliderValueChange(_ sender: UISlider) {
         navigationAppearance.backgroundAlpha = CGFloat(sender.value)
+    }
+
+    @IBAction func pushToNext() {
+        let viewController = storyboard?.instantiateViewController(withIdentifier: "ViewController")
+        navigationController?.pushViewController(viewController!, animated: true)
     }
 }
